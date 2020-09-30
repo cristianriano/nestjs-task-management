7f09dee8232231e7d582bcbb1484f12d2dbecfaf
Cristian Riano <cristiancarm11@gmail.com>
2020-09-30 13:01:48 +0200

    Add Signup endpoint and validate password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private repo: UserRepository
   ) {}
+
+  async signUp(dto: AuthCredentialsDto): Promise<void> {
+    await this.repo.signUp(dto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,17 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
+  async signUp(dto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = dto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    await user.save();
+  }
 }
